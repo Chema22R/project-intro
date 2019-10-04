4161f5d394b39ecded36db240008837e1be0acc6
Chema <chema22r@gmail.com>
2019-10-04 21:47:46 +0200

    Improved the responsive design on mobile devices

--- src/projectIntro.js
+++ src/projectIntro.js
@@ -5,6 +5,7 @@ export function init() {
 
     document.getElementById("projectIntroContainer").style.display = "flex";
     document.getElementById("projectIntroContent").style.maxWidth = document.getElementById("projectIntroImg").offsetWidth + "px";
+    (isMobile() && window.innerHeight < 1000 && window.innerWidth < 1000) ? document.getElementById("projectIntroContent").classList.add("mobile") : null;
     document.getElementById("projectIntroText").style.display = "unset";
     document.getElementById("projectIntroText").scrollTop = 0;
 
@@ -32,4 +33,8 @@ export function fadeOut() {
         document.getElementById("projectIntroContainer").style.display = "none";
         document.body.classList.remove("noScroll");
     }, 500);
+}
+
+function isMobile() {
+    return navigator.userAgent.match(/Android|BlackBerry|iPhone|iPod|Opera Mini|IEMobile/i);
 }
