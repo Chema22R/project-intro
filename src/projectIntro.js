6fe8e62d8e0c0ba8ef0c100556d563696160a631
Jose Maria Rivera <chema22r@gmail.com>
2019-10-05 22:56:07 +0200

    017 scrollbar delay mobile (#18)
    * Timeouts removed on mobile devices
    
    * Modified the way to detect mobile devices

--- src/projectIntro.js
+++ src/projectIntro.js
@@ -1,16 +1,18 @@
 "use strict";
 
+const mobileType = navigator.userAgent.match(/Android|BlackBerry|iPhone|iPod|Opera Mini|IEMobile/i) && navigator.userAgent.match(/Mobile/i);
+
 export function init() {
     document.body.classList.add("noScroll");
 
     document.getElementById("projectIntroContainer").style.display = "flex";
     document.getElementById("projectIntroContent").style.maxWidth = document.getElementById("projectIntroImg").offsetWidth + "px";
-    (isMobile() && window.innerHeight < 1100 && window.innerWidth < 1100) ? document.getElementById("projectIntroContent").classList.add("mobile") : null;
+    mobileType ? document.getElementById("projectIntroContent").classList.add("mobile") : null;
     document.getElementById("projectIntroText").style.display = "unset";
     document.getElementById("projectIntroText").scrollTop = 0;
 
-    setTimeout(() => document.getElementById("projectIntroContent").style.maxWidth = "900px", 500);
-    setTimeout(() => document.getElementById("projectIntroText").style.overflow = "auto", 1500);
+    setTimeout(() => document.getElementById("projectIntroContent").style.maxWidth = "900px", mobileType ? 0 : 500);
+    setTimeout(() => document.getElementById("projectIntroText").style.overflow = "auto", mobileType ? 0 : 2000);
 }
 
 
@@ -33,8 +35,4 @@ export function fadeOut() {
         document.getElementById("projectIntroContainer").style.display = "none";
         document.body.classList.remove("noScroll");
     }, 500);
-}
-
-function isMobile() {
-    return navigator.userAgent.match(/Android|BlackBerry|iPhone|iPod|Opera Mini|IEMobile/i);
 }
