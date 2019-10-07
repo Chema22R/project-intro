5b835ef36a77f337e4fdb76a43787de2f537dae7
Jose Maria Rivera <chema22r@gmail.com>
2019-10-07 12:22:27 +0200

    Min width controlled statically during init animation (#22)

--- src/projectIntro.js
+++ src/projectIntro.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const mobileType = navigator.userAgent.match(/Android|BlackBerry|iPhone|iPod|Opera Mini|IEMobile/i) && navigator.userAgent.match(/Mobile/i);
+const projectIntroTextInitialMinWidth = (window.innerWidth > 900) ? 600 : (window.innerWidth > 700) ? (600 - (900 - window.innerWidth)) : window.innerWidth;
 
 export function init() {
     document.body.classList.add("noScroll");
@@ -9,10 +10,14 @@ export function init() {
     document.getElementById("projectIntroContent").style.maxWidth = document.getElementById("projectIntroImg").offsetWidth + "px";
     mobileType ? document.getElementById("projectIntroContent").classList.add("mobile") : null;
     document.getElementById("projectIntroText").style.display = "unset";
+    document.getElementById("projectIntroText").style.minWidth = `${projectIntroTextInitialMinWidth}px`;
     document.getElementById("projectIntroText").scrollTop = 0;
 
     setTimeout(() => document.getElementById("projectIntroContent").style.maxWidth = "900px", mobileType ? 0 : 500);
-    setTimeout(() => document.getElementById("projectIntroText").style.overflow = "auto", mobileType ? 0 : 2000);
+    setTimeout(() => {
+        document.getElementById("projectIntroText").style.overflow = "auto";
+        document.getElementById("projectIntroText").style.minWidth  = "unset";
+    }, mobileType ? 0 : 2000);
 }
 
 
